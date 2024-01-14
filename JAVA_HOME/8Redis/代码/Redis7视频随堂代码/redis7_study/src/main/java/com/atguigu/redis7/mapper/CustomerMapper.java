4af1edada826ee27047124e05c4fa85d3857d5f5
nrwflqzr-zwhy <wa22201149@stu.aht.edu.cn>
2024-01-14 23:58:40 +0800

    delete redis

--- JAVA_HOME/8Redis/代码/Redis7视频随堂代码/redis7_study/src/main/java/com/atguigu/redis7/mapper/CustomerMapper.java
+++
@@ -1,8 +0,0 @@
-package com.atguigu.redis7.mapper;
-
-import com.atguigu.redis7.entities.Customer;
-import tk.mybatis.mapper.common.Mapper;
-
-public interface CustomerMapper extends Mapper<Customer> {
-}
-
